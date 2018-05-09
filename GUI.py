1cef94fdab3a0eb2123c3dc02423f6106840cbd5
Dominik_Laptop <dominik.krystkowiak@student.put.poznan.pl>
2018-05-09 22:41:02 +0200

    Add views and connection CV & PyQT

--- GUI.py
+++ GUI.py
@@ -1,61 +1,44 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QFileDialog, QLabel, QFormLayout, QSlider
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtCore import *
+from PyQt5.QtWidgets import QApplication, QWidget, QFileDialog, QMainWindow
+from PyQt5.QtGui import QPixmap, QImage
+from PyQt5.uic import loadUi
+from Puzzle_stacker import resize_image, get_contours, crop_image, read_image
 
 
-class App(QWidget):
+def display_image(image):
+    if len(image.shape) == 3:
+        if(image.shape[2]) == 4:
+            qformat = QImage.Format_RGBA8888
+        else:
+            qformat = QImage.Format_RGB888
+    img = QImage(image, image.shape[1], image.shape[0], image.strides[0], qformat)
+    img = img.rgbSwapped()
+    return img
+
+
+class Main(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        self.title = 'Puzzle Stacker'
-        self.left = 100
-        self.top = 100
-        self.width = 500
-        self.height = 500
-        self.fbox = QFormLayout()
-        self.image_load = False
-        self.initUI()
+        loadUi('views/main.ui',self) # ścieżka
+        self.prompt_button.setToolTip('Program pokazuje podpowiedzi na podstawie ułożonego obrazu')
+        self.prompt_button.clicked.connect(self.open_prompt_window)
+        self.solve_button.setToolTip('Program ułoży automatycznie puzzle')
+        self.solve_button.clicked.connect(self.open_auto_solver_window)
+        self.quit_button.setToolTip('Wyłączenie programu')
+        self.quit_button.clicked.connect(QApplication.instance().quit)
 
-    def initUI(self):
-        self.clearLayout(self.fbox)
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
-        button_hint = QPushButton('Podpowiadarka', self)
-        button_hint.clicked.connect(self.hintUI)
-        button_quit = QPushButton('Zakończ', self)
-        button_quit.setToolTip('Wyłączenie programu')
-        button_quit.clicked.connect(QApplication.instance().quit)
-        self.fbox.addRow(button_hint)
-        self.fbox.addRow(button_quit)
-        self.setLayout(self.fbox)
-        self.show()
+    def open_prompt_window(self):
+        self.ui = PromptWindow()
+        self.ui.show()
+        self.hide()
 
+    def open_auto_solver_window(self):
+        self.ui = AutoSolverWindow()
+        self.ui.show()
+        self.hide()
 
-    def hintUI(self):
-        self.clearLayout(self.fbox)
-        self.image_load = False
-        self.shuffle_image = QLabel(self)
-        self.solved_image = QLabel(self)
-        self.fbox.addRow(self.shuffle_image, self.solved_image)
-        button_open_shuffle = QPushButton('Rozsypane puzzle', self)
-        button_open_shuffle.setToolTip('Podaj zdjęcie z rozsypanymi puzzlami')
-        button_open_shuffle.clicked.connect(self.file_open_shuffle)
-        button_open_solved = QPushButton('Ułożone puzzle', self)
-        button_open_solved.setToolTip('Podaj zdjęcie z ułożonymi puzzlami')
-        button_open_solved.clicked.connect(self.file_open_solved)
-        self.fbox.addRow(button_open_shuffle, button_open_solved)
-        button_return = QPushButton('Powrót', self)
-        button_return.setToolTip('Powrót do ekranu głównego')
-        button_return.clicked.connect(self.initUI)
-        self.button_next = QPushButton('Dalej', self)
-        self.button_next.setToolTip('Przejście do wybrania parametrów')
-        self.button_next.setEnabled(False)
-        self.button_next.clicked.connect(self.contoursUI)
-        self.fbox.addRow(button_return, self.button_next)
-        self.setLayout(self.fbox)
-        self.show()
-
+    '''
     def clearLayout(self, layout):
         if layout is not None:
             while layout.count():
@@ -65,61 +48,226 @@ class App(QWidget):
                     widget.deleteLater()
                 else:
                     self.clearLayout(item.layout())
+    '''
+
+
+class PromptWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        loadUi('views/prompt.ui', self) # ścieżka
+        self.images_load = False
+        self.open_shuffle_button.setToolTip('Podaj zdjęcie z rozsypanymi puzzlami')
+        self.open_shuffle_button.clicked.connect(self.file_open_shuffle)
+        self.open_solved_button.setToolTip('Podaj zdjęcie z ułożonymi puzzlami')
+        self.open_solved_button.clicked.connect(self.file_open_solved)
+        self.back_button.setToolTip('Powrót do ekranu głównego')
+        self.back_button.clicked.connect(self.open_main_window)
+        self.next_button.setToolTip('Przejście do wybrania parametrów')
+        self.next_button.clicked.connect(self.open_trackbars_window)
+
+    def open_main_window(self):
+        self.ui = Main()
+        self.ui.show()
+        self.hide()
+
+    def open_trackbars_window(self):
+        self.ui = TrackbarsWindow(self.path_to_shuffle_image, self.path_to_solved_image)
+        self.ui.show()
+        self.hide()
 
     def file_open_shuffle(self):
         options = QFileDialog.Options()
-        fileName, _ = QFileDialog.getOpenFileName(self, "Podaj zdjęcie z rozsypanymi puzzlami", "",
-                                                  "Image files (*.jpg)", options=options)
-        if fileName:
-            pixmap = QPixmap(fileName)
+        self.path_to_shuffle_image, _ = QFileDialog.getOpenFileName(self, 'Podaj zdjęcie z rozsypanymi puzzlami', '',
+                                                  'Image files (*.jpg)', options=options)
+        if self.path_to_shuffle_image:
+            image = display_image(resize_image(self.path_to_shuffle_image, 315, 215))
+            pixmap = QPixmap(image)
             self.shuffle_image.setPixmap(pixmap)
-            if self.image_load == True:
-                self.button_next.setEnabled(True)
+            if self.images_load:
+                self.next_button.setEnabled(True)
             else:
-                self.image_load = True
+                self.images_load = True
 
     def file_open_solved(self):
         options = QFileDialog.Options()
-        fileName, _ = QFileDialog.getOpenFileName(self, "Podaj zdjęcie z ułożonymi puzzlami", "",
-                                                  "Image files (*.jpg)", options=options)
-        if fileName:
-            pixmap = QPixmap(fileName)
+        self.path_to_solved_image, _ = QFileDialog.getOpenFileName(self, 'Podaj zdjęcie z ułożonymi puzzlami', '',
+                                                  'Image files (*.jpg)', options=options)
+        if self.path_to_solved_image:
+            image = display_image(resize_image(self.path_to_solved_image, 315, 215))
+            pixmap = QPixmap(image)
             self.solved_image.setPixmap(pixmap)
-            if self.image_load == True:
-                self.button_next.setEnabled(True)
+            if self.images_load:
+                self.next_button.setEnabled(True)
             else:
-                self.image_load = True
+                self.images_load = True
 
-    def contoursUI(self):
-        self.clearLayout(self.fbox)
-        self.threshold1 = QLabel("0")
-        self.fbox.addRow(self.threshold1)
-        self.slider_threshold1 = QSlider(Qt.Horizontal)
-        self.slider_threshold1.setMinimum(0)
-        self.slider_threshold1.setMaximum(255)
-        self.slider_threshold1.setValue(0)
-        self.slider_threshold1.setTickPosition(QSlider.TicksBelow)
-        self.slider_threshold1.setTickInterval(1)
-        self.slider_threshold1.valueChanged.connect(self.valuechange)
-        self.fbox.addRow(self.slider_threshold1)
-        self.threshold2 = QLabel("0")
-        self.fbox.addRow(self.threshold2)
-        self.slider_threshold2 = QSlider(Qt.Horizontal)
-        self.slider_threshold2.setMinimum(0)
-        self.slider_threshold2.setMaximum(255)
-        self.slider_threshold2.setValue(0)
-        self.slider_threshold2.setTickPosition(QSlider.TicksBelow)
-        self.slider_threshold2.setTickInterval(1)
-        self.slider_threshold2.valueChanged.connect(self.valuechange)
-        self.fbox.addRow(self.slider_threshold2)
-        self.setLayout(self.fbox)
-        self.show()
 
-    def valuechange(self):
-        self.threshold1.setText(str(self.slider_threshold1.value()))
+class TrackbarsWindow(QWidget):
+    def __init__(self, path_to_shuffle_image, path_to_solved_image):
+        super().__init__()
+        loadUi('views/trackbars.ui', self) # ścieżka
+        self.path_to_shuffle_image = path_to_shuffle_image
+        self.path_to_solved_image = path_to_solved_image
+        self.min_slider.valueChanged.connect(self.value_change)
+        self.max_slider.valueChanged.connect(self.value_change)
+        self.back_button.setToolTip('Powrót do poprzedniego ekranu')
+        self.back_button.clicked.connect(self.open_prompt_window)
+        self.default_button.setToolTip('Przywróc ustawienia domyślne')
+        self.default_button.clicked.connect(self.restore_default)
+        self.next_button.setToolTip('Przejście do rozwiązania')
+        self.next_button.clicked.connect(self.open_show_results)
+        self.value_change()
+
+    def open_prompt_window(self):
+        self.ui = PromptWindow()
+        self.ui.show()
+        self.hide()
+
+    def open_show_results(self):
+        if self.path_to_solved_image is None:
+            self.ui = ShowOneResult(self.contours, self.path_to_shuffle_image)
+        else:
+            self.ui = ShowResults(self.contours, self.path_to_solved_image, self.path_to_shuffle_image)
+        self.ui.show()
+        self.hide()
+
+    def restore_default(self):
+        self.min_slider.setValue(40)
+        self.max_slider.setValue(70)
+
+    def value_change(self):
+        self.min_label.setText('Wartość min: ' + str(self.min_slider.value()))
+        self.max_label.setText('Wartość maks: ' + str(self.max_slider.value()))
+        self.contours, image = get_contours(self.path_to_shuffle_image, self.min_slider.value(),
+                                            self.max_slider.value())
+        pixmap = QPixmap(display_image(image))
+        self.contour_image.setPixmap(pixmap)
+
+
+class ShowResults(QWidget):
+    def __init__(self, contours, path_to_solved_image, path_to_shuffle_image):
+        super().__init__()
+        loadUi('views/show_results.ui', self) # ścieżka
+        self.contours = contours
+        self.path_to_shuffle_image = path_to_shuffle_image
+        self.path_to_solved_image = path_to_solved_image
+        self.back_button.setToolTip('Powrót do dostosowania parametrów')
+        self.back_button.clicked.connect(self.open_trackbars_window)
+        self.main_button.setToolTip('Powrót do menu głównego')
+        self.main_button.clicked.connect(self.open_main_window)
+        self.previous_button.setToolTip('Poprzedni obrazek')
+        self.previous_button.clicked.connect(self.previous_image)
+        self.next_button.setToolTip('Następny obrazek')
+        self.next_button.clicked.connect(self.next_image)
+        self.results = []
+        self.position = 0
+        self.prepare_result()
+        self.show_result(self.position)
+
+
+    def open_trackbars_window(self):
+        self.ui = TrackbarsWindow(self.path_to_shuffle_image, self.path_to_solved_image)
+        self.ui.show()
+        self.hide()
+
+    def open_main_window(self):
+        self.ui = Main()
+        self.ui.show()
+        self.hide()
+
+    def previous_image(self):
+        if self.position == 0:
+            self.position = len(self.results) - 1
+        else:
+            self.position = self.position - 1
+        self.show_result(self.position)
+
+    def next_image(self):
+        if self.position == len(self.results) - 1:
+            self.position = 0
+        else:
+            self.position = self.position + 1
+        self.show_result(self.position)
+
+    def prepare_result(self):
+        #for contour in self.contours:
+            #image = crop_image(self.path_to_shuffle_image, contour, i)
+            #image = resize_image(image, 340, 710)
+            #self.results.append(display_image(image))
+        for i in range(0,11):
+            image = read_image('test_images/' + str(i) + '.jpg') # ścieżka
+            image = resize_image(image, 340, 710)
+            self.results.append(display_image(image))
+
+    def show_result(self, index):
+        pixmap = QPixmap(self.results[index])
+        self.show_image.setPixmap(pixmap)
+
+
+class AutoSolverWindow(QWidget): # prawdopodobnie klasa bazowa dla promptwindow
+    def __init__(self):
+        super().__init__()
+        loadUi('views/auto_solver.ui', self)  # ścieżka
+        self.open_shuffle_button.setToolTip('Podaj zdjęcie z rozsypanymi puzzlami')
+        self.open_shuffle_button.clicked.connect(self.file_open_shuffle)
+        self.back_button.setToolTip('Powrót do ekranu głównego')
+        self.back_button.clicked.connect(self.open_main_window)
+        self.next_button.setToolTip('Przejście do wybrania parametrów')
+        self.next_button.clicked.connect(self.open_trackbars_window)
+
+    def open_main_window(self):
+        self.ui = Main()
+        self.ui.show()
+        self.hide()
+
+    def open_trackbars_window(self):
+        self.ui = TrackbarsWindow(self.path_to_shuffle_image, None)
+        self.ui.show()
+        self.hide()
+
+    def file_open_shuffle(self):
+        options = QFileDialog.Options()
+        self.path_to_shuffle_image, _ = QFileDialog.getOpenFileName(self, 'Podaj zdjęcie z rozsypanymi puzzlami',
+                                                                    '',
+                                                                    'Image files (*.jpg)', options=options)
+        if self.path_to_shuffle_image:
+            image = display_image(resize_image(self.path_to_shuffle_image, 315, 215))
+            pixmap = QPixmap(image)
+            self.shuffle_image.setPixmap(pixmap)
+            self.next_button.setEnabled(True)
+
+
+class ShowOneResult(QWidget):
+    def __init__(self, contours, path_to_shuffle_image):
+        super().__init__()
+        loadUi('views/show_result.ui', self) # ścieżka
+        self.contours = contours
+        self.path_to_shuffle_image = path_to_shuffle_image
+        self.back_button.setToolTip('Powrót do dostosowania parametrów')
+        self.back_button.clicked.connect(self.open_trackbars_window)
+        self.main_button.setToolTip('Powrót do menu głównego')
+        self.main_button.clicked.connect(self.open_main_window)
+        #self.show_result()
+
+
+    def open_trackbars_window(self):
+        self.ui = TrackbarsWindow(self.path_to_shuffle_image, None)
+        self.ui.show()
+        self.hide()
+
+    def open_main_window(self):
+        self.ui = Main()
+        self.ui.show()
+        self.hide()
+
+    def show_result(self):
+        image = read_image('all.jpg')  # ścieżka
+        pixmap = QPixmap(resize_image(image, 340, 710))
+        self.show_image.setPixmap(pixmap)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = App()
+    main = Main()
+    main.show()
     sys.exit(app.exec_())
--- Puzzle_stacker.py
+++ Puzzle_stacker.py
@@ -2,109 +2,81 @@
 import numpy as np
 
 
+def read_image(path):
+    return cv2.imread(path)
+
+def resize_image(data, label_height, label_width):
+    if type(data) == str:
+        image = cv2.imread(data)
+        resize = image
+    else:
+        image = data
+        resize = data
+    x = 0.9
+    y = 0.9
+    while True:
+        height, width, _ = resize.shape
+        if height > label_height or width > label_width:
+            resize = cv2.resize(image, (0,0), fx=x, fy=y)
+            x = x - 0.05
+            y = y - 0.05
+        else:
+            break
+    return resize
+
+
 def blur_image(image, amount=3):
     kernel = np.ones((amount, amount), np.float32) / (amount**2)
     return cv2.filter2D(image, -1, kernel)
 
-def get_contours():
 
-    def progowanie(x):
-        print("Progowanie: " + str(x))
-
-    def rozmycie(x):
-        print("rozmycie: " + str(x))
-
-    img = cv2.imread('pies.jpg', 0)
-    img_c = cv2.imread('pies.jpg')
-
-    cv2.namedWindow('image')
-
-    cv2.createTrackbar('progowanie1', 'image', 39, 255, progowanie)
-
-    cv2.createTrackbar('progowanie2', 'image', 38, 255, progowanie)
-
-    cv2.createTrackbar('rozmycie', 'image', 1, 7, rozmycie)
-
-    imgshow = img
-
-    while (1):
-        cv2.imshow('image', imgshow)
-        k = cv2.waitKey(1) & 0xFF
-        if k == 27:
-            break
-        progowanie1 = cv2.getTrackbarPos('progowanie1', 'image')
-
-        progowanie2 = cv2.getTrackbarPos('progowanie2', 'image')
-
-        blur = cv2.getTrackbarPos('rozmycie', 'image')
-
-        if blur % 2 == 0:
-            blur = blur + 1
-        bluredImg = cv2.GaussianBlur(img, (blur, blur), 1.5)
-        imgshow = cv2.Canny(bluredImg, progowanie1, progowanie2)
+def get_contours(path_to_shuffle_image, min_value, max_value):
+    image_gray = cv2.imread(path_to_shuffle_image, 0)
+    image_color = cv2.imread(path_to_shuffle_image)
+    image_blurred = cv2.GaussianBlur(image_gray, (1, 1), 1.5)
+    image_canny = cv2.Canny(image_blurred, min_value, max_value)
+    _, contours, _ = cv2.findContours(blur_image(image_canny), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(image_color, contours, -1, (0, 255, 0), 3)
+    big_contours = []
+    for contour in contours:
+        if cv2.contourArea(contour)>3000:
+            big_contours.append(contour)
+    image_color = resize_image(image_color, 261, 711)
+    return big_contours, image_color
 
 
-    im2, contours, hierarchy = cv2.findContours(blur_image(imgshow), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(img_c, contours, -1, (0, 255, 0), 3)
-    cv2.imshow('dst', img_c)
-    if cv2.waitKey(0) & 0xff == 27:
-        cv2.destroyAllWindows()
-    for con in contours:
-        if cv2.contourArea(con)>3000:
-            crop_image(con)
-
-
-def mask_image(contour):
-    image = cv2.imread('pies.jpg', -1)
+def crop_image(path, contour, i):
+    image = cv2.imread(path)
     mask = np.zeros(image.shape, dtype=np.uint8)
     roi_corners = np.array(contour, dtype=np.int32)
     channel_count = image.shape[2]
     ignore_mask_color = (255,) * channel_count
     cv2.fillConvexPoly(mask, roi_corners, ignore_mask_color)
-    masked_image = cv2.bitwise_and(image, mask)
-    cv2.imshow('dst', masked_image)
-    if cv2.waitKey(0) & 0xff == 27:
-        cv2.destroyAllWindows()
-
-
-def crop_image(contour):
-    image = cv2.imread('pies.jpg', -1)
-    mask = np.zeros(image.shape, dtype=np.uint8)
-    roi_corners = np.array(contour, dtype=np.int32)
-    channel_count = image.shape[2]
-    ignore_mask_color = (255,) * channel_count
-    cv2.fillConvexPoly(mask, roi_corners, ignore_mask_color)
-    masked_image = cv2.bitwise_and(image, mask)
-    epsilon = 0.1 * cv2.arcLength(contour, True)
-    print("/n/n/n/n/n/n/n/n/nNew single puzzle:")
-    print(contour)
     rect = cv2.minAreaRect(contour)
-    print("box:")
     box = cv2.boxPoints(rect)
     box = np.int0(box)
-    print(box)
     x1 = int(box[0][0])
     x2 = int(box[2][0])
     y1 = int(box[1][1])
     y2 = int(box[0][1])
-    # print(rect[0][0])
-    # print(rect[0][1])
-    # print(rect[1][0])
-    # print(rect[1][1])
-    print(x1)
-    print(x2)
-    print(y1)
-    print(y2)
+    contour_image = image[y1:y2, x1:x2]
+    cv2.imwrite(str(i) + '.jpg', contour_image)
+    #return contour_image
+    #cv2.drawContours(image, [box], 0, (0, 0, 255), 2)
 
-    print(box)
-    crop_img = image[y1:y2, x1:x2]
-    cv2.imshow("cropped", crop_img)
-    cv2.waitKey(0)
-    cv2.drawContours(image, [box], 0, (0, 0, 255), 2)
-    print("/n/n/n/n/n/n/n/n/n")
-    cv2.imshow('dst', image)
-    if cv2.waitKey(0) & 0xff == 27:
-        cv2.destroyAllWindows()
 
-if __name__ == "__main__":
-    get_contours()
+# if __name__ == "__main__":
+#     image = cv2.imread('pies.jpg', -1)
+#     contours, _ = get_contours('pies.jpg', 40,70)
+#     i = 0
+#     for con in contours:
+#         crop_image('pies.jpg', con, i)
+#         i = i + 1
+    # image = resize_image(image, 261, 711)
+    # cv2.imshow("cropped", image)
+    # if cv2.waitKey(0) & 0xff == 27:
+    #     cv2.destroyAllWindows()
+    # im = resize_image(image)
+    # cv2.imshow("cropped", im)
+    # if cv2.waitKey(0) & 0xff == 27:
+    #     cv2.destroyAllWindows()
